976bf5b8b403695c2e9fe38329f9c4d2517ea2a2
willian emboava <w.emboava.o@gmail.com>
2020-07-10 11:00:27 -0300

    created tweet list component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,21 +1,97 @@
-import React from "react";
-import { Text } from "react-native";
+import React, { useState } from "react";
+import { FlatList } from "react-native";
 
-import { Container, SearchWrapper, SearchInput } from "./styles";
+import Icon from "react-native-vector-icons/Feather";
+
+import {
+  Container,
+  Content,
+  Header,
+  SearchWrapper,
+  SearchInput,
+  TweetList,
+  TweetContent,
+  TweetText,
+  UserImage,
+  TweetWrapper,
+  UserName,
+} from "./styles";
 
 const Home = () => {
+  const [posts, setPosts] = useState([
+    {
+      user: {
+        screen_name: "bluehost",
+        name: "Bluehost",
+        profile_image_url:
+          "http://pbs.twimg.com/profile_images/1268165217709707265/A4sv4SQ2_normal.jpg",
+      },
+      id: "1280878322038329300",
+      text:
+        "Me: Do I need a semicolon?\nC++: Yes.\nPython: No.\nJavascript: \n\n#Coding #100DaysOfCode #WebDev https://t.co/6FlhqOlN8m",
+      retweet_count: 37,
+    },
+    {
+      user: {
+        screen_name: "ZDNet",
+        name: "ZDNet",
+        profile_image_url:
+          "http://pbs.twimg.com/profile_images/706961982545473536/Ibj46-DX_normal.jpg",
+      },
+      id: "1280509384511107000",
+      text:
+        "Chrome JavaScript timer throttling: Google's tests show it saves up to 2 hours' battery life https://t.co/Ayyysoxxhd",
+      retweet_count: 27,
+    },
+    {
+      user: {
+        screen_name: "LambdaSchool",
+        name: "Lambda School",
+        profile_image_url:
+          "http://pbs.twimg.com/profile_images/1280174472578777095/t2I8Ho16_normal.png",
+      },
+      id: "1280916312865333200",
+      text:
+        "Our Full-Stack Web curriculum is an immersive, online program to prepare you for a successful career in #webdev:… https://t.co/qAIpe8xlBF",
+      retweet_count: 18,
+    },
+    {
+      user: {
+        screen_name: "AjaxTips",
+        name: "Ajax Tips",
+        profile_image_url:
+          "http://pbs.twimg.com/profile_images/3201468625/21156b53c6ccfbbcbfbb2ad2d122c76b_normal.png",
+      },
+      id: "1281216142690959400",
+      text: "Buy vs. build for web features https://t.co/lmxWF2NbUp",
+      retweet_count: 0,
+    },
+  ]);
+
   return (
     <Container>
-      <SearchWrapper>
-        <SearchInput />
-      </SearchWrapper>
-      <Text
-        style={{
-          color: "#fff",
-        }}
-      >
-        Dash
-      </Text>
+      <Header>
+        <SearchWrapper>
+          <Icon name="search" size={22} color="#8899a6" />
+          <SearchInput />
+        </SearchWrapper>
+      </Header>
+
+      <Content>
+        <TweetList
+          data={posts}
+          keyExtractor={(post) => post.id}
+          renderItem={({ item: post }) => (
+            <TweetContent>
+              <UserImage source={{ uri: post.user.profile_image_url }} />
+              <TweetWrapper>
+                <UserName>{post.user.name}</UserName>
+                <TweetText>{post.text}</TweetText>
+              </TweetWrapper>
+            </TweetContent>
+          )}
+        />
+      </Content>
     </Container>
   );
 };
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -2,15 +2,65 @@ import styled from "styled-components/native";
 import { getStatusBarHeight } from "react-native-iphone-x-helper";
 
 export const Container = styled.View`
-  padding: 25px;
+  flex: 1;
   padding-top: ${getStatusBarHeight() + 25}px;
+`;
+
+export const Content = styled.View`
+  flex: 1;
+  padding: 15px 0;
+`;
+
+export const Header = styled.View`
+  padding: 0 15px;
 `;
 
 export const SearchWrapper = styled.View`
   background-color: #253341;
-  border-radius: 20px;
+  border-radius: 30px;
+  padding: 5px 15px;
+
+  flex-direction: row;
+  align-items: center;
 `;
 
 export const SearchInput = styled.TextInput`
+  flex: 1;
   padding: 10px;
+  color: #fff;
+`;
+
+export const TweetList = styled.FlatList`
+  flex: 1;
+`;
+
+export const TweetContent = styled.View`
+  flex-direction: row;
+  padding: 15px 15px;
+  border-bottom-width: 1px;
+  border-bottom-color: rgb(56, 68, 77);
+`;
+
+export const TweetText = styled.Text`
+  flex: 1;
+  color: #fff;
+  font-size: 15px;
+`;
+
+export const UserImage = styled.Image`
+  width: 50px;
+  height: 50px;
+  border-radius: 25px;
+  margin-right: 15px;
+`;
+
+export const TweetWrapper = styled.View`
+  flex: 1;
+`;
+
+export const UserName = styled.Text`
+  color: #fff;
+  font-size: 16px;
+  font-weight: bold;
+  margin-bottom: 3px;
 `;
